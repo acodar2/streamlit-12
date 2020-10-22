be285a8f62472f72abbd91c06aa37cdfd6e23ba1
acodar2 <36757400+acodar2@users.noreply.github.com>
2020-10-22 15:32:02 +0800

    Update crypto-price-app.py

--- crypto-price-app.py
+++ crypto-price-app.py
@@ -23,7 +23,7 @@ image = Image.open('logo.jpg')
 
 st.image(image, width = 500)
 
-st.title('Crypto Price App')
+st.title('Bitcoin')
 st.markdown("""
 This app retrieves cryptocurrency prices for the top 100 cryptocurrency from the **CoinMarketCap**!
 
